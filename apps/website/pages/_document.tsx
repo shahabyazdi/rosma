31230d7dae4eacaa1a4185275a5a9378039348fb
Shahab <sh.yazdi@gmail.com>
2023-05-12 15:00:17 +0330

    the deploy command was removed from the scripts

--- apps/website/pages/_document.tsx
+++ apps/website/pages/_document.tsx
@@ -13,10 +13,6 @@ export default function Document(props: DocumentProps) {
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300&family=Vazirmatn:wght@300;400&display=swap"
           rel="stylesheet"
         />
-        <meta
-          name="google-site-verification"
-          content="eN9hWwtvlYdXL_37ZU2ML8AUY8685Fw3O98T5vidr-4"
-        />
       </Head>
       <body>
         <Main />
