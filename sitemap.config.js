9c1aa17573c0a5744c49c67718b18961a7f00fb2
Shahab <sh.yazdi@gmail.com>
2026-04-14 10:28:56 +1000

    update sitemap config

--- sitemap.config.js
+++ sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://rosma.dev',
+  siteUrl: 'https://www.rosma.dev',
   sourceDir: 'dist/apps/website/.next',
   outDir: 'dist/apps/website/exported',
   exclude: ['/en', '/en/*'],
